0dff4b400094e21949c85fd39aecffeed557876a
oscarbenn <57632650+oscarbenn@users.noreply.github.com>
2022-05-24 00:47:42 +0700

    deleting server file flom sts

---
+++ kodehive/src/main/java/com/kodehive/KodehiveApplication.java
@@ -0,0 +1,13 @@
+package com.kodehive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodehiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodehiveApplication.class, args);
+	}
+
+}
---
+++ kodehive/src/test/java/com/kodehive/KodehiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodehive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodehiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
