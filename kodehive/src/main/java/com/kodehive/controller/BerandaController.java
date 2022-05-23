737b32f4658ff90d60e168ea3580988fc34d4074
oscarbenn <57632650+oscarbenn@users.noreply.github.com>
2022-05-24 00:42:06 +0700

    configuration started spring

---
+++ kodehive/src/main/java/com/kodehive/controller/BerandaController.java
@@ -0,0 +1,16 @@
+package com.kodehive.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BerandaController {
+	
+	@RequestMapping("/beranda")
+	public String beranda() {
+		String html = "beranda";
+		return html;
+	}
+
+}
